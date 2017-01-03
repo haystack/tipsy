515331eec7984c5fde4aaea2f269829c5dc522d7
rht <rhtbot@gmail.com>
2017-01-03 09:40:11 +0400

    Use trusty beta for travis env

--- chrome-extension/test/configure.js
+++ chrome-extension/test/configure.js
@@ -9,20 +9,20 @@ ExtensionDriver.prototype.navigate = function(url) {
 
 var id = 'bpngoepojmffegnjicpfjcakgajpmenk';
 
-before(function() {
+before(function(done) {
   this.timeout(20000);
   this.environment = 'chrome';
+
+  var test = this;
 
   var service = new chrome.ServiceBuilder(chromeDriver.path).build();
   chrome.setDefaultService(service);
 
   var options = new chrome.Options();
-  options.addExtensions(path.resolve('chrome-extension/dist/tipsy.crx'));
-
-  var driver = new chrome.Driver(options, service);
-
-  driver.manage().timeouts().implicitlyWait(10000);
-  this.extensionDriver = new ExtensionDriver(driver, id);
-
-  return this.extensionDriver.navigate('html/index.html');
+  options.addExtensions(path.resolve('chrome-extension/dist/tipsy.crx'), function() {
+    test.driver = new chrome.Driver(options, service);
+    //driver.manage().timeouts().implicitlyWait(1000);
+    test.extensionDriver = new ExtensionDriver(test.driver, id);
+    test.extensionDriver.navigate('html/index.html').then(done);
+  });
 });
