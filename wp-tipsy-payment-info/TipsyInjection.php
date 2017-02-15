0647e9d66dd3a1c784f9717fef5e25e416582b5f
Daniel Aleksandersen <code@daniel.priv.no>
2017-02-15 18:24:30 +0100

    Unique prefix for options and functions
    WordPress Plugin Directory requires all functions and options to
    use a unique prefix to avoid collision with core or other plugins.
    No upgrade code, so notify users in update message.
    
    Removed generic 'Tipsy' string in favor of the blog's own name.
    Could use author name, but would require more logic for pages
    without a given author or multi-author pages.
    
    Use `esc_html` to escape user input.

--- wp-tipsy-payment-info/TipsyInjection.php
+++ wp-tipsy-payment-info/TipsyInjection.php
@@ -8,44 +8,44 @@ Author: schilippe
 License: GPL
 */
 
-add_option("paypal_email");
-add_option("dwolla_key");
-add_action("admin_menu" , 'wp_admin_options_page');
-add_action("wp_head" , 'inject');
+add_option("tipsy_paypal_email");
+add_option("tipsy_dwolla_key");
+add_action("admin_menu" , 'tipsy_wp_admin_options_page');
+add_action("wp_head" , 'tipsy_inject');
 
-function wp_admin_options_page(){
-	add_management_page("Insert Tipsy Info" , "Insert Tipsy Info" , "administrator" , "Tipsy-Injection" , "inputs");
+function tipsy_wp_admin_options_page(){
+	add_management_page("Insert Tipsy Info" , "Insert Tipsy Info" , "administrator" , "Tipsy-Injection" , "tipsy_options_input");
 }
 
-function inputs(){
+function tipsy_options_input(){
 	$inputs  = "<div><h2>Insert Tipsy Payment Info</h2></br>" ;
 	$inputs .= "<form method='post' action='options.php'>" . wp_nonce_field('update-options');
-	$inputs .= "<p>Enter your PayPal email: <br><input type='email' value = '" . get_option(paypal_email) . "' name= 'paypal_email'/></input></p><br/>" ;
-	$inputs .= "<p>Enter your Dwolla key : <br> <input type='text' value =  '" . get_option(dwolla_key) . "' name= 'dwolla_key'/></input></p><br/>" ;
+	$inputs .= "<p>Enter your PayPal email: <br><input type='email' value = '" . esc_html(get_option(tipsy_paypal_email)) . "' name='paypal_email'/></input></p><br/>" ;
+	$inputs .= "<p>Enter your Dwolla key : <br> <input type='text' value =  '" . esc_html(get_option(tipsy_dwolla_key)) . "' name='dwolla_key'/></input></p><br/>" ;
 	$inputs .= "<input type='submit' value='Update' /><input type='hidden' name='action' value='update' />
 				<input type='hidden' name='page_options' value='paypal_email, dwolla_key' /></form></div>"; 
 	echo $inputs;
 }
 
-function inject($content){
-    	$ppe = get_option(paypal_email);
-	$dwk = get_option(dwolla_key);
+function tipsy_inject($content){
+    	$ppe = esc_html(get_option(tipsy_paypal_email));
+	$dwk = esc_html(get_option(tipsy_dwolla_key));
 	
 	if ($ppe === '' && $dwk === '' ) {
 		return; 
 	}
-	$string = "<link rel='author' name='Tipsy'";
+	$string = '<link rel="author" name="' . bloginfo('name') . '"';
 
 	if ($ppe !== '') {
-		$string .= " data-paypal='".$ppe . "'";
+		$string .= ' data-paypal="' . $ppe . '"';
 	}
 
 	if ($dwk !== '') {
-		$string .= " data-dwolla='".$dwk . "'";
+		$string .= ' data-dwolla="' . $dwk . '"';
 	}
 	
 	$string .= ">\n";
-	echo $string; 
+	print($string); 
 	return;
 }
 ?>
