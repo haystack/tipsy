d5cc6c9c4018d475fe466643590df53304f52251
Daniel Aleksandersen <code@daniel.priv.no>
2017-02-18 02:20:55 +0100

    Fix my mistakes in the WordPress plugin

--- wordpress-plugin/tipsy-payments/tipsy-payments.php
+++ wordpress-plugin/tipsy-payments/tipsy-payments.php
@@ -14,16 +14,16 @@ add_action("admin_menu" , 'tipsy_wp_admin_options_page');
 add_action("wp_head" , 'tipsy_inject');
 
 function tipsy_wp_admin_options_page(){
-	add_management_page("Insert Tipsy Info" , "Insert Tipsy Info" , "administrator" , "Tipsy-Injection" , "tipsy_options_input");
+	add_management_page("Tipsy Payment Info" , "Tipsy Payment Info", "administrator" , "tipsy-payments" , "tipsy_options_input");
 }
 
 function tipsy_options_input(){
 	$inputs  = "<div><h2>Insert Tipsy Payment Info</h2></br>" ;
 	$inputs .= "<form method='post' action='options.php'>" . wp_nonce_field('update-options');
-	$inputs .= "<p>Enter your PayPal email: <br><input type='email' value = '" . esc_html(get_option(tipsy_paypal_email)) . "' name='paypal_email'/></input></p><br/>" ;
-	$inputs .= "<p>Enter your Dwolla key : <br> <input type='text' value =  '" . esc_html(get_option(tipsy_dwolla_key)) . "' name='dwolla_key'/></input></p><br/>" ;
+	$inputs .= "<p>Enter your PayPal email: <br><input type='email' value = '" . esc_html(get_option(tipsy_paypal_email)) . "' name='tipsy_paypal_email'/></p><br/>" ;
+	$inputs .= "<p>Enter your Dwolla key : <br> <input type='text' value =  '" . esc_html(get_option(tipsy_dwolla_key)) . "' name='tipsy_dwolla_key'/></p><br/>" ;
 	$inputs .= "<input type='submit' value='Update' /><input type='hidden' name='action' value='update' />
-				<input type='hidden' name='page_options' value='paypal_email, dwolla_key' /></form></div>"; 
+				<input type='hidden' name='page_options' value='tipsy_paypal_email,tipsy_dwolla_key' /></form></div>";
 	echo $inputs;
 }
 
@@ -34,7 +34,7 @@ function tipsy_inject($content){
 	if ($ppe === '' && $dwk === '' ) {
 		return; 
 	}
-	$string = '<link rel="author" name="' . bloginfo('name') . '"';
+	$string = '<link rel="author" name="' . get_bloginfo('name') . '"';
 
 	if ($ppe !== '') {
 		$string .= ' data-paypal="' . $ppe . '"';
